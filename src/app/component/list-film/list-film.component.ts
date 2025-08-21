3a4f02528bd35aad78d597a07b3bd3837057d0fe
Altior1 <thomas.rud@outlook.fr>
2025-08-21 10:11:03 +0200

    generation composants

---
+++ src/app/component/list-film/list-film.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-film',
+  imports: [],
+  templateUrl: './list-film.component.html',
+  styleUrl: './list-film.component.css'
+})
+export class ListFilmComponent {
+
+}
