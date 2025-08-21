b6b9fec91d0f48a6f9ad7182862e73ed990d987b
Altior1 <thomas.rud@outlook.fr>
2025-08-21 10:32:32 +0200

    implémentation getMovies

--- src/app/service/film.service.ts
+++ src/app/service/film.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { Movie } from '../interface/movie';
+import { HttpClient } from '@angular/common/http';
+import { tap, map } from 'rxjs';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class FilmService {
-
-  constructor() { }
+  private totalCount: string | null = null;
+  private url: string = "http://localhost:3000/movies";
+  constructor(private http: HttpClient) { }
+  getMovies(page: number, limit: number): Observable<Movie[]> {
+    return this.http.get<Movie[]>(`${this.url}?_page=${page}&_limit=${limit}`, { observe: "response" }).
+      pipe(
+        tap((res) => this.totalCount = res.headers.get("X-Total-Count")),
+        map((response) => {
+          return response.body.map((data) => {
+            let m: Movie = data;
+            return m;
+          });
+        })
+      );
+  }
 }
