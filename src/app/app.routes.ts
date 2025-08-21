9af825745952fd598d7c918783672bc13d0f2b1f
Altior1 <thomas.rud@outlook.fr>
2025-08-21 10:43:00 +0200

    routes films et films/id

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListFilmComponent } from './component/list-film/list-film.component';
+import { DetailFilmComponent } from './component/detail-film/detail-film.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'films', component: ListFilmComponent },
+    { path: '', redirectTo: '/films', pathMatch: 'full' },
+    { path: 'films/:id', component: DetailFilmComponent }
+];
