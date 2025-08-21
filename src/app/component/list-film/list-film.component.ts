a64d1896add4ab1650021bf15a3d466c05c89460
Altior1 <thomas.rud@outlook.fr>
2025-08-21 10:43:08 +0200

    composant list film

--- src/app/component/list-film/list-film.component.ts
+++ src/app/component/list-film/list-film.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { FilmService } from '../../service/film.service';
+import { nextTick } from 'process';
+import { error } from 'console';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-list-film',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './list-film.component.html',
   styleUrl: './list-film.component.css'
 })
 export class ListFilmComponent {
+  public movies: Movie[] = [];
+  constructor(private filmService: FilmService) {
 
+  }
+  ngOnInit() {
+    this.filmService.getMovies(1, 10).subscribe({
+      next: (data) => {
+        this.movies = data;
+      },
+      error: (err) => {
+        console.error('Error fetching movies:', err);
+      },
+      complete: () => {
+        console.log('Movie fetching complete');
+      }
+    });
+  }
 }
